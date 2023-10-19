30f71fec0d6cc7ae3b80b1142bdf35703022390b
GagoChad <gagika57@gmail.com>
2023-10-19 13:34:05 +0500

    init; create hero section

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+	return (
+		<section>
+			<h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Hello, I&apos;m  Gagik</h1>
+			<p className='text-[#ADB7BE] text-lg lg_text-xl'>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+			</p>
+		</section>
+	)
+}
+
+export default Hero
