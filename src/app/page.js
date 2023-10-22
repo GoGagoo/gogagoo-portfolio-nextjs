f41d9eea16f3d4c1743b4f05a4d5ba052a10c21a
GagoChad <gagika57@gmail.com>
2023-10-22 22:20:00 +0500

    create project section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import AboutSection from './components/AboutSection'
 import Hero from './components/Hero'
 import Navbar  from './components/Navbar'
+import ProjectSection from './components/ProjectSection'
 
 export default function Home() {
 	return (
@@ -9,6 +10,7 @@ export default function Home() {
 			<div className='container mt-24 mx-auto px-12 py-4'>
 				<Hero />
 				<AboutSection />
+				<ProjectSection />
 			</div>
 		</main>
 	)
