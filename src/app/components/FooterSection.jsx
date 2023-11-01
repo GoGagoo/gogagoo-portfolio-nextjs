aea20c5a8fe781178c0f669084699cd75ae14b5c
GagoChad <gagika57@gmail.com>
2023-11-01 20:23:10 +0500

    fix(*): changed footer's element's margins

--- src/app/components/FooterSection.jsx
+++ src/app/components/FooterSection.jsx
@@ -7,9 +7,9 @@ const FooterSection = () => {
 			className='container items-center p-8 flex justify-between'
 			href={"/"}
 			>
-				<span className='text-4xl sm:text-7xl text-[#ADB7BE] hover:text-white font-semibold hover:-rotate-180 transition duration-1000'>G⅁</span>
-				<span className='font-semibold text-1xl sm:text-2xl text-[#ADB7BE] scale-100 rounded-2xl bg-neutral-800 px-2 py-1 hover:text-white transition-transform duration-500 hover:scale-125'>Gagik Antonyan</span>
-				<p className='text-slate-600 text-base sm:text-1xl'>{new Date().getFullYear()} ® All Right Reserved</p>
+				<span className='text-4xl sm:text-7xl text-[#ADB7BE] hover:text-white font-semibold hover:-rotate-180 transition duration-1000 mx-4'>G⅁</span>
+				<span className='font-semibold text-1xl sm:text-2xl text-[#ADB7BE] scale-100 rounded-2xl bg-neutral-800 px-2 py-1 hover:text-white transition-transform duration-500 hover:scale-125 mx-8'>Gagik Antonyan</span>
+				<p className='text-slate-600 text-base sm:text-1xl mx-8'>{new Date().getFullYear()} ® All Right Reserved</p>
 			</Link>
 		</footer>
 	)
