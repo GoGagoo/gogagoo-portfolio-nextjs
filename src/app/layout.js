bf5bf2052126bb4d4ef74e34570983d8c2867a55
GagoChad <gagika57@gmail.com>
2023-11-04 19:44:34 +0500

    feat(*): add beauty scrollbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-lt-installed="true">
+    <html lang="en" className='scrollable' data-lt-installed="true">
       <body className={inter.className}>{children}</body>
     </html>
   )
